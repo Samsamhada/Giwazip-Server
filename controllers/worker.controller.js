567424aceb2215318c774c4850aaa19ee77149d8
taek0622 <min4644@gmail.com>
2022-11-05 23:54:33 +0900

    Feat:sparkles:: #5 Add Worker update Method
    - 한 업자의 정보를 수정하는 기능 추가

--- controllers/worker.controller.js
+++ controllers/worker.controller.js
@@ -68,3 +68,27 @@ exports.findOne = (req, res) => {
             });
         });
 };
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+    Worker.update(req.body, {
+        where: { workerID: id },
+    })
+        .then((num) => {
+            if (num == 1) {
+                res.send({
+                    message: "Worker was updated successfully.",
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Worker with id=${id}. Maybe Worker was not found or req.body is empty!`,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: "Error updating Worker with id=" + id,
+            });
+        });
+};

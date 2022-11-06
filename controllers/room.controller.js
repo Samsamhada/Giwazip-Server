aaacbfd516b355be2f58294d9f1f083a7ee7a2a2
taek0622 <min4644@gmail.com>
2022-11-06 23:10:44 +0900

    Feat:sparkles:: #7 Add RandomStringGenerator
    - 랜덤한 문자열을 생성하는 기능 추가

--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -2,6 +2,21 @@ const db = require("../models");
 const Room = db.rooms;
 const Op = db.Sequelize.Op;
 
+    function generateRandomString(stringLength = 6) {
+        const characters =
+            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        let randomString = "";
+
+        for (let i = 0; i < stringLength; i++) {
+            const randomNumber = Math.floor(Math.random() * characters.length);
+            randomString += characters.substring(
+                randomNumber,
+                randomNumber + 1
+            );
+        }
+
+        return randomString;
+    }
 exports.findAll = (req, res) => {
     const title = req.query.title;
     var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;

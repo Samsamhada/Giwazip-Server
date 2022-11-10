4938620fef6f286d3dc621fecd05a5a9ba039fc2
taek0622 <min4644@gmail.com>
2022-11-11 02:21:54 +0900

    Feat:sparkles:: #19 Add worker findOne docs
    - 특정 업자 조회에 대한 swagger 명세 추가

--- routes/worker.routes.js
+++ routes/worker.routes.js
@@ -21,6 +21,30 @@ module.exports = (app) => {
      */
     router.get("/", workers.findAll);
 
+    /**
+     * @swagger
+     * /workers/{workerID}:
+     *   get:
+     *    summary:
+     *    tags: [Worker]
+     *    parameters:
+     *      - name: workerID
+     *        in: path
+     *        requires: true
+     *        description: workerID는 업자를 식별하기 위한 ID이다.
+     *        schema:
+     *          type: integer
+     *    responses:
+     *      200:
+     *        description: OK
+     *        content:
+     *          application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                  workerID:
+     *                    type: integer
+     */
     router.get("/:id", workers.findOne);
 
     router.put("/:id", workers.update);

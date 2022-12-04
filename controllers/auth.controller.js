268d1dc395e3a4e9bf86aa317580bd2bd6611ff6
taek0622 <min4644@gmail.com>
2022-12-04 22:25:58 +0900

    Chore:wrench:: #16 Modify Token Decode logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,15 +35,31 @@ exports.appleAuth = async (req, res) => {
 
     try {
         console.log("try ok");
-        const base64DecodedText = Buffer.from(
-            base64EncodedText,
-            "base64"
-        ).toString("utf8");
+        const base64DecodedText = Buffer.from(token, "base64").toString("utf8");
         console.log("Base64 Decoded Text : " + base64DecodedText);
         const decodeToken = jwt.decode(base64DecodedText);
         console.log("decodeToken: " + decodeToken);
-        // const tokenDecode = jwt.decode(token);
-        // console.log("token decode: " + tokenDecode);
+        const email = decodeToken.email;
+        console.log("email: " + decodeToken.email);
+        console.log("sub: " + decodeToken.sub);
+
+        const base64DecodedCode = Buffer.from(code, "base64").toString("utf8");
+        console.log("Base64 Decoded Code : " + base64DecodedCode);
+        const decodeCode = jwt.decode(base64DecodedCode);
+        console.log("decodeCode: " + decodeCode);
+
+        const responese2 = await auth.accessToken(base64DecodedCode);
+        console.log("response2: " + responese2);
+        const response = await auth.accessToken(decodeToken);
+        console.log("decode token: " + response);
+
+        // const base64DeocdedCode = Buffer.from(code, "base64").toString("utf8");
+        // console.log("code base: " + base64DeocdedCode);
+        // const response2 = await auth.accessToken(code);
+        // console.log("response2 ok");
+        // const response = await auth.accessToken(base64DeocdedCode);
+        // console.log("response ok");
+
         // console.log("token decode header: " + tokenDecode.header);
         // console.log("token decode");
         // const response = await auth.accessToken(code);
@@ -60,49 +76,55 @@ exports.appleAuth = async (req, res) => {
         if (!name) {
             name = "익명의 유저";
         }
+        console.log(name);
 
         if (!email) {
             email = "";
         }
+        console.log(email);
 
         // Create a Worker
         const worker = {
-            userIdentifier: sub,
+            userIdentifier: decodeToken.sub,
             name: name,
             email: email,
             number: req.body.number,
         };
 
-        Worker.create(worker)
-            .then((data) => {
-                res.send(data);
-                console.log(
-                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                        chalk.bgGreen("Success:") +
-                        " Worker 테이블에 새로운 데이터가 성공적으로 추가되었습니다. (IP: " +
-                        (req.header("X-FORWARDED-FOR") ||
-                            req.socket.remoteAddress) +
-                        ")"
-                );
-            })
-            .catch((err) => {
-                res.status(500).send({
-                    message:
-                        err.message ||
-                        "Some error occurred while creating the Worker.",
-                });
-                console.log(
-                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                        chalk.bgRed("Error:") +
-                        " " +
-                        err.message ||
-                        "Some error occurred while creating the Worker. (IP: " +
-                            (req.header("X-FORWARDED-FOR") ||
-                                req.socket.remoteAddress) +
-                            ")"
-                );
-            });
+        console.log(worker);
+        res.status(200).send(worker);
+
+        // Worker.create(worker)
+        //     .then((data) => {
+        //         res.send(data);
+        //         console.log(
+        //             `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+        //                 chalk.bgGreen("Success:") +
+        //                 " Worker 테이블에 새로운 데이터가 성공적으로 추가되었습니다. (IP: " +
+        //                 (req.header("X-FORWARDED-FOR") ||
+        //                     req.socket.remoteAddress) +
+        //                 ")"
+        //         );
+        //     })
+        //     .catch((err) => {
+        //         res.status(500).send({
+        //             message:
+        //                 err.message ||
+        //                 "Some error occurred while creating the Worker.",
+        //         });
+        //         console.log(
+        //             `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+        //                 chalk.bgRed("Error:") +
+        //                 " " +
+        //                 err.message ||
+        //                 "Some error occurred while creating the Worker. (IP: " +
+        //                     (req.header("X-FORWARDED-FOR") ||
+        //                         req.socket.remoteAddress) +
+        //                     ")"
+        //         );
+        //     });
     } catch (e) {
         console.log("Token is invalid or error occurred");
+        res.status(400).send({ message: "Error" });
     }
 };

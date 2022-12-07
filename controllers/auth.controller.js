0a0099c7bc2978e702832f529b1ef422f0de03ae
taek0622 <min4644@gmail.com>
2022-12-07 22:25:04 +0900

    Feat:sparkles:: #16 Add login logic
    - 토큰을 이용한 로그인 로직 추가
    - 기존의 테스트용 잘못된 로직 삭제

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,6 +19,7 @@ dotenv.config();
 exports.appleAuth = async (req, res) => {
     let code = req.body.code;
     let token = req.body.token;
+    let name = req.body.name;
     console.log("code: " + code);
     console.log("token: " + req.body.token);
     if (!code) {
@@ -32,99 +33,54 @@ exports.appleAuth = async (req, res) => {
         );
         return;
     }
+    const base64DecodedText = Buffer.from(token, "base64").toString("utf8");
+    const decodeToken = jwt.decode(base64DecodedText);
+    const email = decodeToken.email;
 
-    try {
-        console.log("try ok");
-        const base64DecodedText = Buffer.from(token, "base64").toString("utf8");
-        console.log("Base64 Decoded Text : " + base64DecodedText);
-        const decodeToken = jwt.decode(base64DecodedText);
-        console.log("decodeToken: " + decodeToken);
-        const email = decodeToken.email;
-        console.log("email: " + decodeToken.email);
-        console.log("sub: " + decodeToken.sub);
+    if (!name) {
+        name = "익명의 유저";
+    }
 
-        const base64DecodedCode = Buffer.from(code, "base64").toString("utf8");
-        console.log("Base64 Decoded Code : " + base64DecodedCode);
-        const decodeCode = jwt.decode(base64DecodedCode);
-        console.log("decodeCode: " + decodeCode);
+    if (!email) {
+        email = "";
+    }
 
-        const responese2 = await auth.accessToken(base64DecodedCode);
-        console.log("response2: " + responese2);
-        const response = await auth.accessToken(decodeToken);
-        console.log("decode token: " + response);
+    // Create a Worker
+    const worker = {
+        userIdentifier: decodeToken.sub,
+        name: name,
+        email: email,
+        number: req.body.number,
+    };
 
-        // const base64DeocdedCode = Buffer.from(code, "base64").toString("utf8");
-        // console.log("code base: " + base64DeocdedCode);
-        // const response2 = await auth.accessToken(code);
-        // console.log("response2 ok");
-        // const response = await auth.accessToken(base64DeocdedCode);
-        // console.log("response ok");
-
-        // console.log("token decode header: " + tokenDecode.header);
-        // console.log("token decode");
-        // const response = await auth.accessToken(code);
-        // console.log("response ok");
-        // const idToken = jwt.decode(response.id_token);
-        // console.log("idToken ok");
-        const name = req.body.name;
-        console.log("name ok");
-        // const email = idToken.email;
-        // console.log("email ok");
-        // const sub = idToken.sub;
-        // console.log("sub ok");
-
-        if (!name) {
-            name = "익명의 유저";
-        }
-        console.log(name);
-
-        if (!email) {
-            email = "";
-        }
-        console.log(email);
-
-        // Create a Worker
-        const worker = {
+    Worker.findOrCreate({
+        where: { userIdentifier: decodeToken.sub },
+        defaults: {
             userIdentifier: decodeToken.sub,
             name: name,
             email: email,
             number: req.body.number,
-        };
-
-        console.log(worker);
-        res.status(200).send(worker);
-
-        // Worker.create(worker)
-        //     .then((data) => {
-        //         res.send(data);
-        //         console.log(
-        //             `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-        //                 chalk.bgGreen("Success:") +
-        //                 " Worker 테이블에 새로운 데이터가 성공적으로 추가되었습니다. (IP: " +
-        //                 (req.header("X-FORWARDED-FOR") ||
-        //                     req.socket.remoteAddress) +
-        //                 ")"
-        //         );
-        //     })
-        //     .catch((err) => {
-        //         res.status(500).send({
-        //             message:
-        //                 err.message ||
-        //                 "Some error occurred while creating the Worker.",
-        //         });
-        //         console.log(
-        //             `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-        //                 chalk.bgRed("Error:") +
-        //                 " " +
-        //                 err.message ||
-        //                 "Some error occurred while creating the Worker. (IP: " +
-        //                     (req.header("X-FORWARDED-FOR") ||
-        //                         req.socket.remoteAddress) +
-        //                     ")"
-        //         );
-        //     });
-    } catch (e) {
-        console.log("Token is invalid or error occurred");
-        res.status(400).send({ message: "Error" });
-    }
+        },
+    }).then((user, created) => {
+        res.status(200).send(user[0]);
+        if (user[1]) {
+            console.log(
+                `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+                    chalk.bgGreen("Success:") +
+                    " 회원 가입에 성공했습니다. (IP: " +
+                    (req.header("X-FORWARDED-FOR") ||
+                        req.socket.remoteAddress) +
+                    ")"
+            );
+        } else {
+            console.log(
+                `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+                    chalk.bgGreen("Success:") +
+                    " 로그인에 성공했습니다. (IP: " +
+                    (req.header("X-FORWARDED-FOR") ||
+                        req.socket.remoteAddress) +
+                    ")"
+            );
+        }
+    });
 };

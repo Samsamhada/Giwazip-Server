226f5a34567ac868fbef73a5f222b132498ecf4a
taek0622 <min4644@gmail.com>
2023-01-27 03:03:32 +0900

    Feat:sparkles:: #33 Add User-Room Model
    - DB 구조 변경에 따라 업자와 방을 N:M 매칭하기 위한 User-Room 모델 추가

---
+++ models/userroom.model.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, Sequelize) => {
+    const UserRoom = sequelize.define(
+        "userroom",
+        {
+            userRoomID: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                field: "user_room_id",
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            userID: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                field: "user_id",
+            },
+            roomID: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                field: "room_id",
+            },
+        },
+        {
+            timestamps: false,
+            freezeTableName: true,
+            tableName: "user_room",
+        }
+    );
+
+    // Worker.associate = (models) => {
+    //     Worker.hasMany(models.Room, {
+    //         foreignKey: "workerID",
+    //         sourceKey: "workerID",
+    //     });
+    // };
+
+    return UserRoom;
+};

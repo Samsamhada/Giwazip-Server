2a7cbc74b705c848292d9138a1b5bbbca03df57b
taek0622 <min4644@gmail.com>
2023-01-27 05:44:03 +0900

    Chore:wrench:: #35 Modify Photo Creation Function
    - 사진을 추가하는 기능 변경 (변수명 변경)

--- routes/photo.routes.js
+++ routes/photo.routes.js
@@ -7,15 +7,15 @@ module.exports = (app) => {
 
     router.post("/", imageUploader.single("photo"), photos.create);
 
-    router.get("/", photos.findAll);
+    // router.get("/", photos.findAll);
 
-    router.get("/:id", photos.findOne);
+    // router.get("/:id", photos.findOne);
 
-    router.get("/post/:id", photos.findByPostID);
+    // router.get("/post/:id", photos.findByPostID);
 
-    router.put("/:id", photos.update);
+    // router.put("/:id", photos.update);
 
-    router.delete("/:id", photos.delete);
+    // router.delete("/:id", photos.delete);
 
     app.use("/giwazip/photos", router);
 };

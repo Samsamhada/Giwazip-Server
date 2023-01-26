fb9fa1090dbb36ac842c4a0de108824aea0ae1a4
taek0622 <min4644@gmail.com>
2023-01-27 05:43:35 +0900

    Chore:wrench:: #35 Modify Post Creation Function
    - 새로운 게시물을 추가하는 기능 수정

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,19 +5,19 @@ module.exports = (app) => {
 
     router.post("/", posts.create);
 
-    router.get("/", posts.findAll);
+    // router.get("/", posts.findAll);
 
-    router.get("/:id", posts.findOne);
+    // router.get("/:id", posts.findOne);
 
-    router.get("/room/:id", posts.findByRoomID);
+    // router.get("/room/:id", posts.findByRoomID);
 
-    router.get("/photo/room/:id", posts.findByRoomIDWithPhoto);
+    // router.get("/photo/room/:id", posts.findByRoomIDWithPhoto);
 
-    router.get("/category/:category", posts.findByCategory);
+    // router.get("/category/:category", posts.findByCategory);
 
-    router.put("/:id", posts.update);
+    // router.put("/:id", posts.update);
 
-    router.delete("/:id", posts.delete);
+    // router.delete("/:id", posts.delete);
 
     app.use("/giwazip/posts", router);
 };

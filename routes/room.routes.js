d93908119c1ccfa423172dce8ec4eebdcfc398a5
taek0622 <min4644@gmail.com>
2023-01-27 05:37:27 +0900

    Chore:wrench:: #35 Modify Room Creation Function
    - 방 정보를 추가하는 기능 수정

--- routes/room.routes.js
+++ routes/room.routes.js
@@ -5,19 +5,19 @@ module.exports = (app) => {
 
     router.post("/", rooms.create);
 
-    router.get("/", rooms.findAll);
+    // router.get("/", rooms.findAll);
 
-    router.get("/:id", rooms.findOne);
+    // router.get("/:id", rooms.findOne);
 
-    router.get("/worker/:id", rooms.findByWorkerID);
+    // router.get("/worker/:id", rooms.findByWorkerID);
 
-    router.get("/invite_code/:inviteCode", rooms.findByInviteCode);
+    // router.get("/invite_code/:inviteCode", rooms.findByInviteCode);
 
-    router.put("/:id", rooms.update);
+    // router.put("/:id", rooms.update);
 
-    router.put("/invite_code/:inviteCode", rooms.updateClientNumber);
+    // router.put("/invite_code/:inviteCode", rooms.updateClientNumber);
 
-    router.delete("/:id", rooms.delete);
+    // router.delete("/:id", rooms.delete);
 
     app.use("/giwazip/rooms", router);
 };

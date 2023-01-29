7c27f15c5bc28b6ee3da1a6cf381b9796f1caab0
taek0622 <min4644@gmail.com>
2023-01-29 17:30:44 +0900

    Feat:sparkles:: #37 Add finding specific category
    - 특정 카테고리 조회 기능 추가

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -121,3 +121,57 @@ exports.findAll = (req, res) => {
         );
     }
 };
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    if (req.header("API-Key") == apiKey) {
+        Category.findByPk(id)
+            .then((data) => {
+                if (data) {
+                    res.send(data);
+                    console.log(
+                        `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+                            chalk.bgGreen("Success:") +
+                            ` Category 테이블의 id=${id}번 데이터를 성공적으로 조회했습니다. (IP: ` +
+                            (req.header("X-FORWARDED-FOR") ||
+                                req.socket.remoteAddress) +
+                            ")"
+                    );
+                } else {
+                    res.status(400).send({
+                        message: `Category 테이블에서 id=${id}번 데이터를 찾을 수 없습니다.`,
+                    });
+                    console.log(
+                        `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+                            chalk.bgRed("Error:") +
+                            ` Category 테이블에서 id=${id}번 데이터를 찾을 수 없습니다.` +
+                            " (IP: " +
+                            (req.header("X-FORWARDED-FOR") ||
+                                req.socket.remoteAddress) +
+                            ")"
+                    );
+                }
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: `Category 테이블의 id=${id}번 데이터를 조회하는 중에 문제가 발생했습니다.`,
+                });
+                console.log(
+                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+                        chalk.bgRed("Error:") +
+                        ` Category 테이블의 id=${id}번 데이터를 조회하는 중에 문제가 발생했습니다.`
+                );
+            });
+    } else {
+        res.status(401).send({ message: "Connection Fail" });
+        console.log(
+            `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
+                chalk.bgRed("Error: ") +
+                ` Connection Fail at GET /categories/${id}` +
+                " (IP: " +
+                (req.header("X-FORWARDED-FOR") || req.socket.remoteAddress) +
+                ")"
+        );
+    }
+};

d43226fb24d5cdef5585e66a0151729c8a534ab3
taek0622 <min4644@gmail.com>
2023-02-01 03:14:33 +0900

    Chore:wrench:: #40 Modify message and Log Style
    - 응답 메시지 및 로그 스타일 변경

--- controllers/worker.controller.js
+++ controllers/worker.controller.js
@@ -4,12 +4,15 @@ const Op = db.Sequelize.Op;
 const dotenv = require("dotenv");
 const moment = require("moment");
 const chalk = require("chalk");
+const purple = chalk.hex("#9900ff");
 
 dotenv.config();
 
 const apiKey = process.env.API_KEY;
 
 exports.create = (req, res) => {
+    const IP = req.header("X-FORWARDED-FOR") || req.socket.remoteAddress;
+
     if (req.header("API-Key") == apiKey) {
         // Validate request
 
@@ -23,12 +26,11 @@ exports.create = (req, res) => {
                 message: "Worker 테이블의 필수 정보가 누락 되었습니다!",
             });
             console.log(
-                `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                    chalk.bgRed("Error:") +
-                    " Worker 테이블의 필수 데이터를 포함하지 않고 Create를 시도했습니다. (IP: " +
-                    (req.header("X-FORWARDED-FOR") ||
-                        req.socket.remoteAddress) +
-                    ")"
+                `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] 🔴${chalk.red(
+                    "Error:"
+                )} ${chalk.yellow(
+                    "Worker 테이블"
+                )}의 필수 데이터를 포함하지 않고 Create를 시도했습니다. (IP: ${IP})`
             );
             return;
         }
@@ -45,45 +47,42 @@ exports.create = (req, res) => {
             .then((data) => {
                 res.send(data);
                 console.log(
-                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                        chalk.bgGreen("Success:") +
-                        " Worker 테이블에 새로운 데이터가 성공적으로 추가되었습니다. (IP: " +
-                        (req.header("X-FORWARDED-FOR") ||
-                            req.socket.remoteAddress) +
-                        ")"
+                    `[${moment().format(
+                        "YYYY-MM-DD HH:mm:ss.SSS"
+                    )}] 🟢${chalk.green("Success:")} ${chalk.yellow(
+                        "Worker 테이블"
+                    )}에 새로운 데이터가 성공적으로 추가되었습니다. (IP: ${IP})`
                 );
             })
             .catch((err) => {
                 res.status(500).send({
                     message:
-                        err.message ||
-                        "새로운 업자 정보를 추가하는 중에 문제가 발생했습니다.",
+                        "새로운 Worker를 추가하는 중에 문제가 발생했습니다.",
+                    detail: err.message,
                 });
                 console.log(
-                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                        chalk.bgRed("Error:") +
-                        " " +
-                        err.message ||
-                        "새로운 Worker 정보를 추가하는 중에 문제가 발생했습니다. (IP: " +
-                            (req.header("X-FORWARDED-FOR") ||
-                                req.socket.remoteAddress) +
-                            ")"
+                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] 🟣${purple(
+                        "Error:"
+                    )} 새로운 ${chalk.yellow(
+                        "Worker"
+                    )}를 추가하는 중에 문제가 발생했습니다. ${chalk.dim(
+                        "상세정보: " + err.message
+                    )} (IP: ${IP})`
                 );
             });
     } else {
         res.status(401).send({ message: "Connection Fail" });
         console.log(
-            `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                chalk.bgRed("Error:") +
-                " Connection Fail at POST /workers (IP: " +
-                (req.header("X-FORWARDED-FOR") || req.socket.remoteAddress) +
-                ")"
+            `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] 🔴${chalk.red(
+                "Error:"
+            )} Connection Fail at ${chalk.yellow("POST /workers")} (IP: ${IP})`
         );
     }
 };
 
 exports.update = (req, res) => {
     const id = req.params.id;
+    const IP = req.header("X-FORWARDED-FOR") || req.socket.remoteAddress;
 
     if (req.header("API-Key") == apiKey) {
         Worker.update(req.body, {
@@ -94,51 +93,52 @@ exports.update = (req, res) => {
                 if (data[0] == 1) {
                     res.send(data[1][0]);
                     console.log(
-                        `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                            chalk.bgGreen("Success:") +
-                            ` Worker 테이블의 ${id}번 데이터가 성공적으로 수정되었습니다. (IP: ` +
-                            (req.header("X-FORWARDED-FOR") ||
-                                req.socket.remoteAddress) +
-                            ")"
+                        `[${moment().format(
+                            "YYYY-MM-DD HH:mm:ss.SSS"
+                        )}] 🟢${chalk.green("Success:")} ${chalk.yellow(
+                            "Worker 테이블"
+                        )}의 ${chalk.yellow(
+                            id + "번"
+                        )} 데이터가 성공적으로 수정되었습니다. (IP: ${IP})`
                     );
                 } else {
                     res.send({
                         message: `Worker 테이블의 ${id}번 데이터를 수정할 수 없습니다. 해당 데이터를 찾을 수 없거나, request의 body가 비어있습니다.`,
                     });
                     console.log(
-                        `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                            chalk.bgRed("Error:") +
-                            ` Worker 테이블의 ${id}번 데이터를 수정할 수 없습니다. 해당 데이터를 찾을 수 없거나, request의 body가 비어있습니다.` +
-                            " (IP: " +
-                            (req.header("X-FORWARDED-FOR") ||
-                                req.socket.remoteAddress) +
-                            ")"
+                        `[${moment().format(
+                            "YYYY-MM-DD HH:mm:ss.SSS"
+                        )}] 🔴${chalk.red("Error:")} ${chalk.yellow(
+                            "Worker 테이블"
+                        )}의 ${chalk.yellow(
+                            id + "번"
+                        )} 데이터를 수정할 수 없습니다. 해당 데이터를 찾을 수 없거나, request의 body가 비어있습니다. (IP: ${IP})`
                     );
                 }
             })
             .catch((err) => {
                 res.status(500).send({
                     message: `Worker 테이블의 ${id}번 데이터를 수정하는 중에 문제가 발생했습니다.`,
+                    detail: err.message,
                 });
                 console.log(
-                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                        chalk.bgRed("Error:") +
-                        ` Worker 테이블의 ${id}번 데이터를 수정하는 중에 문제가 발생했습니다.` +
-                        " (IP: " +
-                        (req.header("X-FORWARDED-FOR") ||
-                            req.socket.remoteAddress) +
-                        ")"
+                    `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] 🟣${purple(
+                        "Error:"
+                    )} ${chalk.yellow("Worker 테이블")}의 ${chalk.yellow(
+                        id + "번"
+                    )} 데이터를 수정하는 중에 문제가 발생했습니다. ${chalk.dim(
+                        "상세정보: " + err.message
+                    )} (IP: ${IP})`
                 );
             });
     } else {
         res.status(401).send({ message: "Connection Fail" });
         console.log(
-            `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] ` +
-                chalk.bgRed("Error:") +
-                ` Connection Fail at PUT /workers/${id}` +
-                " (IP: " +
-                (req.header("X-FORWARDED-FOR") || req.socket.remoteAddress) +
-                ")"
+            `[${moment().format("YYYY-MM-DD HH:mm:ss.SSS")}] 🔴${chalk.red(
+                "Error:"
+            )} Connection Fail at ${chalk.yellow(
+                "PUT /workers/" + id
+            )} (IP: ${IP})`
         );
     }
 };

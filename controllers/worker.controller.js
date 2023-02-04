31084c3679188eebf6fad3ac72bfc042a8e0db67
taek0622 <min4644@gmail.com>
2023-02-04 22:33:56 +0900

    Fix:beetle:: #56 Modify worker update status
    - Worker를 업데이트하려고 시도하여 실패했을 때, 반환 정보를 더 상세하게 변경

--- controllers/worker.controller.js
+++ controllers/worker.controller.js
@@ -191,6 +191,9 @@ exports.update = (req, res) => {
     const id = req.params.id;
     const IP = req.header(reqHeaderIPField) || req.socket.remoteAddress;
     const userID = req.body.userID;
+    const userIdentifier = req.body.userIdentifier;
+    const name = req.body.name;
+    const email = req.body.email;
 
     if (req.header(reqHeaderAPIKeyField) == apiKey) {
         if (userID) {
@@ -225,9 +228,9 @@ exports.update = (req, res) => {
                             `${id}번`
                         )} 데이터가 성공적으로 수정되었습니다. (IP: ${IP})`
                     );
-                } else {
-                    res.send({
-                        message: `${workerLabel} 테이블의 ${id}번 데이터를 수정할 수 없습니다. 해당 데이터를 찾을 수 없거나, request의 body가 비어있습니다.`,
+                } else if (!userIdentifier && !name && !email) {
+                    res.status(400).send({
+                        message: `${workerLabel} 테이블의 ${id}번 데이터를 수정할 수 없습니다. request의 body가 비어있습니다.`,
                     });
                     console.log(
                         `[${moment().format(
@@ -236,7 +239,20 @@ exports.update = (req, res) => {
                             `${workerLabel} 테이블`
                         )}의 ${chalk.yellow(
                             `${id}번`
-                        )} 데이터를 수정할 수 없습니다. 해당 데이터를 찾을 수 없거나, request의 body가 비어있습니다. (IP: ${IP})`
+                        )} 데이터를 수정할 수 없습니다. request의 body가 비어있습니다. (IP: ${IP})`
+                    );
+                } else {
+                    res.status(404).send({
+                        message: `${workerLabel} 테이블의 ${id}번 데이터를 찾을 수 없습니다.`,
+                    });
+                    console.log(
+                        `[${moment().format(
+                            dateFormat
+                        )}] ${badAccessError} ${chalk.yellow(
+                            `${workerLabel} 테이블`
+                        )}의 ${chalk.yellow(
+                            `${id}번`
+                        )} 데이터를 수정할 수 없습니다. 해당 데이터를 찾을 수 없습니다. (IP: ${IP})`
                     );
                 }
             })

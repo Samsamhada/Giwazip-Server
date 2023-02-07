eacc33df2ec01d7e146b2f32594324698fbfaada
taek0622 <min4644@gmail.com>
2023-02-07 23:15:56 +0900

    Feat:sparkles:: #69 Add Admin Create
    - 새로운 관리자를 추가하는 기능 추가

---
+++ controllers/admin.controller.js
@@ -0,0 +1,84 @@
+const db = require("../models");
+const Admin = db.admins;
+const Op = db.Sequelize.Op;
+const dotenv = require("dotenv");
+const moment = require("moment");
+const chalk = require("chalk");
+const purple = chalk.hex("#9900ff");
+const dateFormat = "YYYY-MM-DD HH:mm:ss.SSS";
+const success = `🟢${chalk.green("Success:")}`;
+const badAccessError = `🔴${chalk.red("Error:")}`;
+const unknownError = `🟣${purple("Error:")}`;
+const reqHeaderIPField = "X-FORWARDED-FOR";
+const reqHeaderAPIKeyField = "x-api-key";
+const asc = "ASC";
+
+dotenv.config();
+
+const apiKey = process.env.ADMIN_KEY;
+
+exports.create = (req, res) => {
+    const IP = req.header(reqHeaderIPField) || req.socket.remoteAddress;
+
+    if (req.header(reqHeaderAPIKeyField) == apiKey) {
+        let id = req.body.id;
+        let pw = req.body.pw;
+        let name = req.body.name;
+
+        if (!id || !pw || !name) {
+            res.status(400).send({
+                message: `${Admin.name} 테이블의 필수 정보가 누락 되었습니다!`,
+            });
+            console.log(
+                `[${moment().format(
+                    dateFormat
+                )}] ${badAccessError} ${chalk.yellow(
+                    `${Admin.name} 테이블`
+                )}의 필수 데이터를 포함하지 않고 Create를 시도했습니다. (IP: ${IP})`
+            );
+            return;
+        }
+
+        const admin = {
+            id: id,
+            pw: pw,
+            name: name,
+            allow_changing: false,
+        };
+
+        Admin.create(admin)
+            .then((data) => {
+                res.status(200).send(data);
+                console.log(
+                    `[${moment().format(dateFormat)}] ${success} ${chalk.yellow(
+                        `${Admin.name} 테이블`
+                    )}에 새로운 데이터가 성공적으로 추가되었습니다. (IP: ${IP})`
+                );
+            })
+            .catch((err) => {
+                res.status(500).send({
+                    message: `새로운 ${Admin.name}를 추가하는 중에 문제가 발생했습니다.`,
+                    detail: err.message,
+                });
+                console.log(
+                    `[${moment().format(
+                        dateFormat
+                    )}] ${unknownError} 새로운 ${chalk.yellow(
+                        Admin.name
+                    )}를 추가하는 중에 문제가 발생했습니다. ${chalk.dim(
+                        `상세정보: ${err.message}`
+                    )} (IP: ${IP})`
+                );
+            });
+    } else {
+        res.status(403).send({ message: "Connection Fail" });
+        console.log(
+            `[${moment().format(
+                dateFormat
+            )}] ${badAccessError} Connection Fail at ${chalk.yellow(
+                "POST /admins"
+            )} (IP: ${IP})`
+        );
+    }
+};
+
---
+++ routes/admin.routes.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+    const admins = require("../controllers/admin.controller.js");
+
+    var router = require("express").Router();
+
+    router.post("/", admins.create);
+
+    app.use("/giwazip/admins", router);
+};
